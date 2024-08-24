255b7b4db2d7f450865670af8ad806d8a2cfbda9
amirzach <2023500147@student.uitm.edu.my>
2024-08-24 10:44:15 +0800

    Chatbot prototype 1

--- Test.py
+++ Test.py
@@ -1,6 +1,28 @@
-import numpy as np
+import random
 
-msg = "Roll a dice!"
-print(msg)
+greetings = ["Hello!", "What's up?!", "Howdy!", "Greetings!"]
+goodbyes = ["Bye!", "Gooddbye!", "See you later!", "See you soon"]
+keywords = ["music", "pet", "book", "game"]
+responses = ["Music is so relaxing!", "Dogs are man's best friend!", "I know about a lot of books.", "I like to play video games."]
 
-print(np.random.randint(1,9))
+print(random.choice(greetings))
+user = input("Say something (or type bye to quit): ")
+user = user.lower()
+
+while (user!= "bye"):
+    keyword_found = False
+    
+    for index in range(len(keywords)):
+        if (keywords[index] in user):
+            print("Bot: "+responses[index])
+            keyword_found = True
+    
+    if (keyword_found == False):
+        new_keyword = input("I'm not sure how to respond to that. What specific keyword should I respond to? ")
+        keywords.append(new_keyword)
+        new_response = input("How should i respond to "+new_keyword+"? ")
+        responses.append(new_response)
+    
+    user = input("Say something (or type bye to quit): ")
+    user = user.lower()
+print(random.choice(goodbyes))
