68033744c9980c200e64f467d3a16735d3d1e185
amirzach <2023500147@student.uitm.edu.my>
2024-08-27 10:42:39 +0800

    Improved prompts

--- Test.py
+++ Test.py
@@ -2,8 +2,8 @@ import random
 
 greetings = ["Hello!", "What's up?!", "Howdy!", "Greetings!"]
 goodbyes = ["Bye!", "Gooddbye!", "See you later!", "See you soon"]
-keywords = ["music", "pet", "book", "game", "created", "sentient"]
-responses = ["Music is so relaxing!", "Dogs are man's best friend!", "I know about a lot of books.", "I like to play video games.", "I was created by Kaz on the 19th of August 2024", "I am not sentient yet because all of my answers have been predetermined by Kaz"]
+keywords = ["music", "pet", "book", "game", "created", "sentient", "capabilities"]
+responses = ["Music is so relaxing!", "Dogs are man's best friend!", "I know about a lot of books.", "I like to play video games.", "I was created by Kaz on the 19th of August 2024", "I am not sentient yet because all of my answers have been predetermined by Kaz", "As far as you're concerned, i am only able to answer simple inquiries"]
 
 print(random.choice(greetings))
 print("I am a chatbot designed for basic conversations with you")
